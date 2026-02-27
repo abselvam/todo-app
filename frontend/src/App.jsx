a310c996a3f7c91b79037d0197785c35078d8219
Abhijith Selvam <abselvam44@gmail.com>
2026-02-27 22:41:17 +0530

    frontend initial setup complete

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import All from "./pages/All";
+import Pending from "./pages/Pending";
+import Completed from "./pages/Completed";
+import InProgress from "./pages/InProgress";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col overflow-hidden">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<All />} />
+        <Route path="/pending" element={<Pending />} />
+        <Route path="/completed" element={<Completed />} />
+        <Route path="/in-progress" element={<InProgress />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
