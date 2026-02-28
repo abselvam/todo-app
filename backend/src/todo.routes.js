56ecac5fcd2759c7da83baf76d4502a92ce2c6fa
Abhijith Selvam <abselvam44@gmail.com>
2026-02-28 12:13:34 +0530

    seeding functionality for bulk data

--- backend/src/todo.routes.js
+++ backend/src/todo.routes.js
@@ -5,6 +5,7 @@ import {
   editTodo,
   getAllTodo,
 } from "./todo.controller.js";
+import Todo from "./model.js";
 
 const router = express.Router();
 
@@ -12,5 +13,14 @@ router.get("/all", getAllTodo);
 router.post("/add", addTodo);
 router.patch("/edit/:id", editTodo);
 router.delete("/delete/:id", deleteTodo);
+// for seeding bulk data through postman
+router.post("/seed", async (req, res) => {
+  try {
+    await Todo.insertMany(req.body);
+    res.status(201).json({ message: "Todos seeded successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Seed failed" });
+  }
+});
 
 export default router;
